8a4027cb7d91fba50b77de24cac2aa6259e6082d
unknown <ynarula678@gmail.com>
2021-08-11 11:59:00 +0530

    footer +  slider  and some fixes

--- js/app.js
+++ js/app.js
@@ -62,5 +62,95 @@ const mailImg = document.createElement('div');
 mailImg.classList.add('mail-img-div');
 mailDiv.appendChild(mailImg);
 const circleMail = document.createElement('div');
+const CircleMail = document.createElement('div');
 circleMail.classList.add('circle-mail');
-mailDiv.appendChild(circleMail)
+CircleMail.classList.add('circle-mail-1');
+mailDiv.appendChild(circleMail);
+mailDiv.appendChild(CircleMail);
+const input = document.querySelector('.input');
+input.addEventListener('click',()=>{
+    input.value = '';
+    input.style.paddingLeft = '2.5vw';
+    input.style.color = '#12143F';
+})
+//slider
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  userData = [
+    {
+        name : 'Jethalal Gada',
+        img  : 'https://static.abplive.com/wp-content/uploads/sites/2/2020/07/27140156/jethalal.jpg?impolicy=abp_images&imwidth=720',
+        review :      
+        `
+        Lorem ipsum dolor sit amet consectetur adipisicing <br>
+        elit. Perferendis impedit dolores natus hic, optio rem
+        `,
+        star : `<i class="fas fa-star"></i> <i class="far fa-star"></i>`
+    },
+    {
+        name : 'Krishnan Iyer',
+        img  : 'https://biographymafia.com/wp-content/uploads/2021/04/YASH_22-1.jpg',
+        review :      
+        `
+        Lorem ipsum dolor sit amet consectetur adipisicing <br>
+        elit. Perferendis impedit dolores natus hic, optio rem
+        `,
+        star : `<i class="fas fa-star"></i> <i class="fas fa-star"></i><i class="fas fa-star"></i> <i class="far fa-star"></i>`
+    },
+    {
+        name : 'Tarak Mehta',
+        img  : 'https://images.jansatta.com/2020/09/Shailesh-Lodha.jpg',
+        review :      
+        `
+        Lorem ipsum dolor sit amet consectetur adipisicing <br>
+        elit. Perferendis impedit dolores natus hic, optio rem
+        `,
+        star : `<i class="fas fa-star"></i> <i class="far fa-star"></i>`
+    },
+    {
+        name : 'Sundar Lal',
+        img  : 'https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/taarak_mehta_ka_ooltah_chashmah_disha_vakanis_onscreen_real_brother_mayur_vakani_extends_rakhi_wishes.jpg',
+        review :      
+        `
+        Lorem ipsum dolor sit amet consectetur adipisicing <br>
+        elit. Perferendis impedit dolores natus hic, optio rem
+        `,
+        star : `<i class="fas fa-star"></i> <i class="far fa-star"></i>`
+    }
+]
+for (var user = 0; user <userData.length; user++){
+    const userSlide = document.createElement('div');
+    userSlide.classList.add('swiper-slide');
+    userSlide.innerHTML = `
+    <img src='${userData[user].img}'>
+    <h2>${userData[user].name}</h2>
+    <p>${userData[user].review}</p>
+    <div class='star-div'>
+        ${userData[user].star}
+    </div>
+    `;
+    document.querySelector('.swiper-wrapper').appendChild(userSlide);
+}
